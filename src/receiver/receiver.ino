1eda6132adaf6bb46a84cd1e86bbfe2c6f0a7e07
jedrek <jedrekszor@wp.pl>
2019-04-04 18:32:59 +0200

    further changes to receiver

--- src/receiver/receiver.ino
+++ src/receiver/receiver.ino
@@ -1,4 +1,5 @@
 #include "LiquidCrystal.h"
+#include "Time.h"
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 int lcd_key     = 0;
@@ -27,6 +28,10 @@ int read_LCD_buttons()
 
 void setup()
 {
+  static unsigned char up_arrow[8] = {0x04, 0x0E, 0x15, 0x04, 0x04, 0x04, 0x04, 0x00};
+static unsigned char down_arrow[8] = {0x00, 0x04, 0x04, 0x04, 0x04, 0x15, 0x0E, 0x04};
+
+
  lcd.begin(16, 2);
  lcd.setCursor(0,0);
  lcd.print("Welcome to");
@@ -41,7 +46,8 @@ void setup()
 void loop()
 {
  lcd_key = read_LCD_buttons();
-
+ 
+ 
  switch (lcd_key)
  {
      case btnRIGHT:
@@ -88,6 +94,84 @@ void loop()
       break;
      }
   }
-  lcd.setCursor(0,0);
-  lcd.print(menuStatus);
+
+  switch(menuStatus)
+  {
+    case 0:   //temperature
+    {
+      lcd.setCursor(0,0);
+      lcd.print("Temperature:   ^");
+      lcd.setCursor(0,1);
+      int temp = 7;
+      lcd.print(temp);
+      lcd.print("\337C                ");
+      lcd.setCursor(15,1);
+      lcd.print("v");
+      break;
+    }
+    case 1:   //humidity
+    {
+      lcd.setCursor(0,0);
+      lcd.println("Humidity:      ^");
+      lcd.setCursor(0,1);
+      int hum = 35;
+      lcd.print(hum);
+      lcd.print("%                ");
+      lcd.setCursor(15,1);
+      lcd.print("v");
+      break;
+    }
+    case 2:   //set alarm
+    {
+      int hour = 12;
+      int minutes = 0;
+      lcd.setCursor(0,0);
+      lcd.println("Set alarm:     ^");
+      lcd.setCursor(0,1);
+      
+      lcd.print(hour);
+      lcd.print(":");
+      if(minutes < 10)
+        lcd.print("0");
+      lcd.print(minutes);
+      lcd.print("             ");
+      lcd.setCursor(15,1);
+      lcd.print("v");
+      break;
+    }
+    case 3:   //show alarms
+    {
+      lcd.setCursor(0,0);
+      lcd.println("Show alarms:   ^");
+      lcd.setCursor(0,1);
+      
+      lcd.print("%                ");
+      lcd.setCursor(15,1);
+      lcd.print("v");
+      break;
+    }
+    case 4:   //current time and date
+    {
+      lcd.setCursor(0,0);
+      lcd.println("Current time:  ^");
+      lcd.setCursor(0,1);
+
+      time_t t = now();
+      if(hour(t) < 10)
+        lcd.print("0");
+      lcd.print(hour(t));
+      lcd.print(":");
+      if(minute(t) < 10)
+        lcd.print("0");
+      lcd.print(minute(t));
+      lcd.print(":");
+      if(second(t) < 10)
+        lcd.print("0");
+      lcd.print(second(t));
+      lcd.print("       ");
+      lcd.setCursor(15,1);
+      lcd.print("v");
+      break;
+    }
+  }
  }
