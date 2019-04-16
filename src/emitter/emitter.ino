07686f9fb3bf142f2866183b10fbbacdb7853990
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2019-04-16 19:52:31 +0200

    Emitter tym razem dobry

--- src/emitter/emitter.ino
+++ src/emitter/emitter.ino
@@ -1,79 +1,64 @@
-#include "LiquidCrystal.h"
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <dht.h>
 
-int lcd_key     = 0;
-int adc_key_in  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
+#define DHT11_PIN 7
 
-int read_LCD_buttons()
-{
- adc_key_in = analogRead(0);
- if (adc_key_in > 1000) return btnNONE;
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 250)  return btnUP; 
- if (adc_key_in < 450)  return btnDOWN; 
- if (adc_key_in < 650)  return btnLEFT; 
- if (adc_key_in < 850)  return btnSELECT;  
- return btnNONE;
-}
+static double CORRECTING_COEFFICIENT = -5.0;
+static int CRYING_THRESHOLD = 700;
+static int SOUND_PIN = A5;
+static int DELAY_TIME = 3000;
+static int TIME_BETWEEN_SOUND_CHECK = 100;
+static int SOUND_CHECKS_AMOUNT = 30;
+const byte address[6] = "00001";
+dht DHT;
+RF24 radio(9, 8);  // CE, CSN
+char message[5] = "    ";
+
 
 void setup()
 {
- lcd.begin(16, 2);
- lcd.setCursor(0,0);
- lcd.print("Welcome to");
- lcd.setCursor(0,1);
- lcd.print("smart-nanny");
- delay(5000);
- lcd.clear();
- lcd.setCursor(0,1);
- lcd.print("dupa");
+  while(!Serial);
+    Serial.begin(9600);
+  radio.begin();
+  pinMode(SOUND_PIN, INPUT);
+  radio.openWritingPipe(address);
+  radio.stopListening();
 }
- 
+
 void loop()
 {
-// lcd.setCursor(9,1);
-// lcd.print(millis()/1000);
-//
-// lcd.setCursor(0,1);
-// lcd_key = read_LCD_buttons();
-//
-// switch (lcd_key)
-// {
-//   case btnRIGHT:
-//     {
-//     lcd.print("RIGHT ");
-//     break;
-//     }
-//   case btnLEFT:
-//     {
-//     lcd.print("LEFT   ");
-//     break;
-//     }
-//   case btnUP:
-//     {
-//     lcd.print("UP    ");
-//     break;
-//     }
-//   case btnDOWN:
-//     {
-//     lcd.print("DOWN  ");
-//     break;
-//     }
-//   case btnSELECT:
-//     {
-//     lcd.print("SELECT");
-//     break;
-//     }
-//     case btnNONE:
-//     {
-//     lcd.print("NONE  ");
-//     break;
-//     }
-//  }
- }
+  clearMessage();
+  getTemp().toCharArray(message, 5);
+  if(isCrying()) message[4] = '1';
+  else message[4] = '0';
+  radio.write(&message, sizeof(message));
+  Serial.println(message);
+}
+
+String getTemp() {
+  String temp = "";
+  int chk = DHT.read11(DHT11_PIN);
+  temp = String((int)(DHT.temperature + CORRECTING_COEFFICIENT)) + String((int)DHT.humidity);
+  return temp;
+}
+
+bool isCrying() {
+  int state;
+  bool isCrying = false;
+  for (int i = 0; i < SOUND_CHECKS_AMOUNT; i++)
+  {
+    state = analogRead(SOUND_PIN);
+    if (state > CRYING_THRESHOLD) isCrying = true;
+    delay(TIME_BETWEEN_SOUND_CHECK);
+  }
+  return isCrying;
+}
+
+void clearMessage()
+{
+  for (int i = 0; i < sizeof(message); i++)
+    message[i] = ' ';  
+}
